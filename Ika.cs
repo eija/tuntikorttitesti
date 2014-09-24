1739c3dd6b6feb9d4a2421a821bc05355bf57867
eija <eija.makipaa@live.com>
2014-09-24 14:06:58 +0300

    Kotitehtävä1

---
+++ Ika.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtava1
+{
+    class Ika
+    {
+        public void laskeika(int nvuosi,int nkk, int svuosi, int skk, out int ikavuosi, out int ikakk)
+        {
+            int ikakuukausina = (12-skk)+ 12*(nvuosi-svuosi)+nkk;
+            ikavuosi= ikakuukausina/12;
+            ikakk = ikakuukausina%12;
+
+        }
+
+    }
+}
